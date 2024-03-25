6496d44e67902abe9b1c51c5559684a2220294bc
Andrew <thum14252@gmail.com>
2024-03-25 15:52:41 +0630

    added explore menu

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Home.css";
 import Header from '../../components/Header/Header';
+import ExplorMenu from '../../components/ExplorMenu/ExplorMenu';
 
 const Home = () => {
+    const [category, setCategory] = useState("All");
     return (
         <div>
             <Header />
+            <ExplorMenu category={category} setCategory={setCategory} />
         </div>
     )
 }
